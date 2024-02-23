473caa7d6aee8a56d290c591fb3f57af4dfe81a5
Konstantin Bobkov <mrglorf@gmail.com>
2024-02-23 22:09:05 +0400

    correct base API path

--- frontend/src/constants/ApiBaseUrl.ts
+++ frontend/src/constants/ApiBaseUrl.ts
@@ -1,10 +1,9 @@
-const { hostname } = window.location;
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_PATH = {
-  BASE:
-    hostname === 'localhost' || '192.168.43.59'
-      ? 'http://localhost:5050/api'
-      : 'https://eslteachertoolkitbe.vercel.app/api',
+  BASE: isProduction
+    ? 'https://eslteachertoolkitbe.vercel.app/api'
+    : 'http://localhost:5050/api',
   SIGN_UP: '/auth/signup',
   SIGN_IN: '/auth/signin',
   CURRENT_USER: '/users/me',
