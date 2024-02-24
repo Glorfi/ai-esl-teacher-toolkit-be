3f7a195946b1627127431cd882e3a7bb8c842d19
Konstantin Bobkov <mrglorf@gmail.com>
2024-02-24 06:31:03 +0400

    fix mobile dashboard menu 2

--- frontend/src/components/MobileMenuDashboard.tsx
+++ frontend/src/components/MobileMenuDashboard.tsx
@@ -70,7 +70,13 @@ export const MobileMenuDashBoard = (): JSX.Element => {
             width={'100%'}
             padding={0}
           >
-            <VStack minH={'100%'} w={'100%'} pb={'20px'}>
+            <VStack
+              minH={
+                'calc(100% - env(safe-area-inset-top) - env(safe-area-inset-bottom)'
+              }
+              w={'100%'}
+              pb={'20px'}
+            >
               <Link as={ReactRouterLink} to={APP_PATHS.DASHBOARD} w={'100%'}>
                 <Button
                   w={'100%'}
