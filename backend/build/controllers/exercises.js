693d747d60bdb203e8aa1dfdda7b4f6f47178e17
Konstantin Bobkov <mrglorf@gmail.com>
2024-02-27 09:34:34 +0400

    new build with gpt request

--- backend/build/controllers/exercises.js
+++ backend/build/controllers/exercises.js
@@ -1,5 +1,7 @@
 import { Exercises, Sentences, Users } from '../db/mongoConnector.js';
 import { NotFound } from '../errors/NotFound.js';
+import { openAIRequest } from '../utils/openAIrequest.js';
+import { GenerationFailed } from '../errors/GenerationFailed.js';
 export const getUserExercises = (req, res, next) => {
     const { _id } = req.user;
     Exercises.find({ owner: _id })
@@ -52,6 +54,70 @@ export const createExercise = (req, res, next) => {
             .then(() => ex.populate('sentenceList').then((newEx) => res.send(newEx)));
     })
         .catch((err) => next(err));
+};
+export const generateExercise = (req, res, next) => {
+    const { _id: owner } = req.user;
+    const { prompt, skill, type } = req.body;
+    openAIRequest(prompt)
+        .then((sentenceList) => {
+        if (!Array.isArray(sentenceList)) {
+            throw new GenerationFailed('AI returned non-array data structure');
+        }
+        const isSentenceListValid = sentenceList.every((item) => item.sentence.includes(item.answer));
+        if (!isSentenceListValid) {
+            throw new GenerationFailed('AI returned wrong data structure');
+        }
+        Exercises.create({ owner, skill, type }).then((ex) => {
+            Users.findById(owner)
+                .then((user) => {
+                if (!user) {
+                    throw new NotFound("User's not found");
+                }
+                user.exercises?.push(ex._id);
+                return user.save();
+            })
+                .catch((err) => next(err));
+            const sentencePromises = sentenceList.map((item) => {
+                const exId = ex._id;
+                const { sentence, answer, hint, options } = item;
+                return Sentences.create({
+                    sentence,
+                    answer,
+                    hint,
+                    options,
+                    exercise: exId,
+                });
+            });
+            return Promise.all(sentencePromises)
+                .then((sentences) => {
+                ex.sentenceList = sentences.map((sentence) => sentence._id);
+                return ex.save();
+            })
+                .then(() => ex.populate('sentenceList').then((newEx) => res.send(newEx)));
+        });
+    })
+        .catch((err) => next(err));
+    // fetch(OPEN_AI_BASE_URL, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Authorization: `Bearer ${process.env.OPEN_AI_KEY}`,
+    //   },
+    //   body: JSON.stringify({
+    //     model: 'gpt-3.5-turbo',
+    //     messages: [{ role: 'user', content: prompt }],
+    //     temperature: 1,
+    //     max_tokens: 500,
+    //   }),
+    // })
+    //   .then((gptData) => gptData.json())
+    //   .then((gptRes: IGptResponse) => {
+    //     const gptMessageString = gptRes.choices[0].message.content;
+    //     return gptMessageString;
+    //   })
+    //   .catch((err) => {
+    //     next(err);
+    //   });
 };
 export const deleteExercise = (req, res, next) => {
     let removedEx;
