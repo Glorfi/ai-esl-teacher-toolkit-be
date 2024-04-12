a8655ebae3f78f851e9749cffca22f76805944cf
Konstantin Bobkov <mrglorf@gmail.com>
2024-04-12 07:13:27 +0400

    trying without build

--- build/errors/GenerationFailed.js
+++
@@ -1,7 +0,0 @@
-export class GenerationFailed extends Error {
-    constructor(message) {
-        super(message);
-        this.statusCode = 406;
-    }
-}
-//# sourceMappingURL=GenerationFailed.js.map
--- build/utils/calculateSimilarityCoefficient.js
+++
@@ -1,8 +0,0 @@
-export const calculateSimilarityCoefficient = (query, resultName) => {
-    const startIndex = resultName.toLowerCase().indexOf(query.toLowerCase());
-    if (startIndex === -1) {
-        return 0;
-    }
-    return 1 / (startIndex + 1);
-};
-//# sourceMappingURL=calculateSimilarityCoefficient.js.map
