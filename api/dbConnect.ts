37ad2faedc2180c191921f401057ad0274a48c3a
Konstantin Bobkov <mrglorf@gmail.com>
2024-07-24 23:54:22 +0400

    trying another mongoConnector as serverless function

---
+++ api/dbConnect.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import exerciseSchema from "../src/db/models/exerciseSchema.js";
+import sentenceSchema from "../src/db/models/sentenceSchema.js";
+import topicSchema from "../src/db/models/topicSchema.js";
+import userSchema from "../src/db/models/userSchema.js";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.NODE_ENV === 'production'
+  ? process.env.MONGO_LINK
+  : 'mongodb://127.0.0.1:27017/exsdb';
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+declare global {
+  var mongoose: any; // This must be a `var` and not a `let / const`
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    console.log('=> using existing database connection');
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    console.log('=> creating new database connection');
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI as string, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+    // cached.conn.models.Users = mongoose.models.users || mongoose.model('users', userSchema);
+    // cached.conn.models.Exercises = mongoose.models.exercises || mongoose.model('exercises', exerciseSchema);
+    // cached.conn.models.Sentences = mongoose.models.sentences || mongoose.model('sentences', sentenceSchema);
+    // cached.conn.models.Topics = mongoose.models.topics || mongoose.model('topics', topicSchema);
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
--- src/db/mongoConnector.ts
+++ src/db/mongoConnector.ts
@@ -4,6 +4,8 @@ import userSchema from './models/userSchema.js';
 import exerciseSchema from './models/exerciseSchema.js';
 import sentenceSchema from './models/sentenceSchema.js';
 import topicSchema from './models/topicSchema.js';
+import dbConnect from '../../api/dbConnect.js';
+import { log } from 'console';
 
 dotenv.config();
 const currentDb =
@@ -11,19 +13,11 @@ const currentDb =
     ? process.env.MONGO_LINK
     : 'mongodb://127.0.0.1:27017/exsdb';
 
-mongoose
-  .connect(currentDb || '', {})
-  .then(() => {
-    console.log('DataBase is Connected');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+await dbConnect()
+  .then(() => console.log('HELLO YOPTA'))
+  .catch(() => console.log('ERROR YOPTA'));
 
-const db = mongoose.connection;
-db.on('error', (err) => {
-  console.error(err);
-});
+
 
 const Users = mongoose.model('users', userSchema);
 const Exercises = mongoose.model('exercises', exerciseSchema);
@@ -31,3 +25,17 @@ const Sentences = mongoose.model('sentences', sentenceSchema);
 const Topics = mongoose.model('topics', topicSchema);
 
 export { Users, Exercises, Sentences, Topics };
+
+// mongoose
+//   .connect(currentDb || '', {})
+//   .then(() => {
+//     console.log('DataBase is Connected');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// const db = mongoose.connection;
+// db.on('error', (err) => {
+//   console.error(err);
+// });
